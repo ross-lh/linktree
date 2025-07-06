924b8310902147599eda3636e83e83ce2529d480
ross-lh <lhbenross@gmail.com>
2025-07-06 17:18:14 -0300

    adding full project to gh

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  /* manual toggle construction 
+  if (html.classList.contains("light")) {
+  html.classList.remove("light")
+  } else {
+  html.classList.add("light")
+  }*/
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "../assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Luiz Ross de óculos em um ambiente claro com fundo cinza escuro"
+    )
+  } else {
+    img.setAttribute("src", "../assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Luiz Ross de óculos escuros em um ambiente escuro com fundo levemente iluminado"
+    )
+  }
+}
