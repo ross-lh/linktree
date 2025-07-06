db0f3159d3d2b5c186af5d12edce77198fb58db4
ross-lh <lhbenross@gmail.com>
2025-07-06 17:52:10 -0300

    adding README.md and fixing images path

--- js/script.js
+++ js/script.js
@@ -12,13 +12,13 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "../assets/avatar-light.png")
+    img.setAttribute("src", "assets/avatar-light.png")
     img.setAttribute(
       "alt",
       "Luiz Ross de óculos em um ambiente claro com fundo cinza escuro"
     )
   } else {
-    img.setAttribute("src", "../assets/avatar.png")
+    img.setAttribute("src", "assets/avatar.png")
     img.setAttribute(
       "alt",
       "Luiz Ross de óculos escuros em um ambiente escuro com fundo levemente iluminado"
